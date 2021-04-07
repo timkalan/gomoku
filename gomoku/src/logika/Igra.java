26ee6b47eb6c8617d47a9fe46311b65405416de1
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2021-04-07 13:57:47 +0200

    ČAST IN SLAVA NAMA!

--- gomoku/src/logika/Igra.java
+++ gomoku/src/logika/Igra.java
@@ -20,28 +20,30 @@ public class Igra {
 		
 		// Iniciraliziramo N-vrste
 		int[][] smer = {{1,0}, {0,1}, {1,1}, {1,-1}};
-		for (int x = 0; x < N; x++) {
-			for (int y = 0; y < N; y++) {
-				for (int[] s : smer) {
-					int dx = s[0];
-					int dy = s[1];
-					// če je skrajno polje terice še na plošči, jo dodamo med terice
-					if ((0 <= x + (N-1) * dx) && (x + (N-1) * dx < N) && 
-						(0 <= y + (N-1) * dy) && (y + (N-1) * dy < N)) {
-						int[] vrsta_x = new int[M];
-						int[] vrsta_y = new int[M];
-						for (int k = 0; k < M; k++) {
-							vrsta_x[k] = x + dx * k;
-							vrsta_y[k] = y + dy * k;
+		for (int i = 0; i<N-M;i++) {
+			for (int j = 0; j<N-M ;j++) {
+				for (int x = 0; x < M; x++) {
+					for (int y = 0; y < M; y++) {
+						for (int[] s : smer) {
+							int dx = s[0];
+							int dy = s[1];
+							// če je skrajno polje terice še na plošči, jo dodamo med terice
+							if ((0 <= x + (M-1) * dx) && (x + (M-1) * dx < M) && 
+								(0 <= y + (M-1) * dy) && (y + (M-1) * dy < M)) {
+								int[] vrsta_x = new int[M];
+								int[] vrsta_y = new int[M];
+								for (int k = 0; k < M; k++) {
+									vrsta_x[k] = x + i + dx * k;
+									vrsta_y[k] = y + j + dy * k;
+								}
+								VRSTE.add(new Vrsta(vrsta_x, vrsta_y));
+							}
 						}
-						VRSTE.add(new Vrsta(vrsta_x, vrsta_y));
 					}
 				}
 			}
 		}
 	}
-	
-
 	// Igralno polje
 	private Polje[][] plosca;
 	
@@ -87,16 +89,19 @@ public class Igra {
 	private Igralec cigavaVrsta(Vrsta t) {
 		int count_X = 0;
 		int count_O = 0;
-		for (int k = 0; k < M && (count_X == 0 || count_O == 0); k++) {
-			switch (plosca[t.x[k]][t.y[k]]) {
-			case O: count_O += 1; break;
-			case X: count_X += 1; break;
-			case PRAZNO: break;
-			}
-		}
-		if (count_O == M) { return Igralec.O; }
-		else if (count_X == M) { return Igralec.X; }
-		else { return null; }
+
+				for (int k = 0; k < M && (count_X == 0 || count_O == 0); k++) {
+					switch (plosca[t.x[k]][t.y[k]]) {
+					case O: count_O += 1; break;
+					case X: count_X += 1; break;
+					case PRAZNO: break;
+					}
+					if (count_O == M) { return Igralec.O; }
+					else if (count_X == M) { return Igralec.X; }
+				}
+			
+		
+		return null;
 	}
 
 	/**
